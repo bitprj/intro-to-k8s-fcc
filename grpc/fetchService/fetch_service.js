cb165267a1152155e670279d5f7f56de6119bdd7
emsesc <emseschen@gmail.com>
2021-09-25 21:53:49 -0400

    add in actual code

--- grpc/fetchService/fetch_service.js
+++ grpc/fetchService/fetch_service.js
@@ -1,7 +1,8 @@
-var PROTO_PATH = __dirname + '/../proto/manipulate.proto';
+var PROTO_PATH = __dirname + '/manipulate.proto';
 
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
+import grpc from '@grpc/grpc-js'
+import protoLoader from '@grpc/proto-loader'
+
 var packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {keepCase: true,
@@ -12,14 +13,131 @@ var packageDefinition = protoLoader.loadSync(
     });
 var manipulate_proto = grpc.loadPackageDefinition(packageDefinition).manipulate;
 
-function main() {
+function requestManipulate(face, hat) {
     target = 'localhost:50051';
     var client = new manipulate_proto.Manipulate(target, grpc.credentials.createInsecure());
 
-    var user = "world";
-    client.manipulateService({username: user}, function(err, response) {
-        console.log('Greeting:', response.base64);
-    });
+    let response = await client.manipulateService({face: face, hat: hat})
+    return response.base64
 }
 
-main();
+import express from 'express'
+import multer from 'multer'
+import mysql from 'mysql2'
+import fetch from 'node-fetch'
+const upload = multer()
+const app = express()
+var router = express.Router();
+const PORT = 1337
+
+// for testing locally: node -r dotenv/config index.js  
+// https://stackoverflow.com/questions/28305120/differences-between-express-router-and-app-get
+
+const HOST = process.env.HOST;
+const PASSWORD = process.env.PASSWORD;
+
+const con = mysql.createConnection({
+    host: HOST,
+    port: '3306',
+    user: "admin",
+    password: PASSWORD,
+});
+
+app.use('/', router)
+
+app.listen(PORT, () => {
+    console.log(`API Gateway started on port ${PORT}`)
+})
+
+router.get('/fetch', upload.any(), async(req, res) => {
+    let style = req.query.style
+    let face = await defaultBoss()
+    let b64Result = ''
+
+    if (style != undefined) {
+        console.log("No custom image, yes style")
+        let hat = await getSpecificHat(style)
+        if (hat == null) {
+            return res.status(400).send({
+                message: 'This hat style does not exist! If you want this style - try submitting it'
+             });             
+        }
+        console.log("Got specific hat")
+        b64Result = await requestManipulate(face, hat)
+        res.send(b64Result)
+    } else {
+        console.log("No custom image, no style")
+        let hat = await getRandomHat()
+        b64Result = await requestManipulate(face, hat)
+        res.send(b64Result)
+    }
+});
+
+router.post('/fetch', upload.any(), async(req, res) => {
+    let style = req.query.style
+    let face = req.files[0].buffer
+    let b64Result = ''
+
+    if (style != undefined) {
+        console.log("Custom image, no style")
+        let hat = await getSpecificHat(style)
+
+        b64Result = await requestManipulate(face, hat)
+    } else {
+        console.log("Custom image, yes style")
+        let hat = await getRandomHat()
+
+        b64Result = await requestManipulate(face, hat)
+    }
+
+    res.send(b64Result)
+});
+
+async function getSpecificHat(style) {
+    var sql = `SELECT * FROM main.images WHERE description='${style}';`;
+    const results = await con.promise().query(sql)
+        .catch(err => console.log(err))
+    
+    let hatList = results[0]
+    console.log(hatList)
+    if (hatList.length == 0){
+        return null
+    }
+
+    let randNum = Math.floor(Math.random() * hatList.length)
+    let hatLink = hatList[randNum].base64
+    console.log(hatLink)
+
+    return Buffer.from(hatLink, "base64")
+}
+
+async function getRandomHat() {
+    // get random hat picture
+    let hats = await listPictures()
+    let hatList = hats[0]
+    console.log(hatList)
+
+    let randNum = Math.floor(Math.random() * hatList.length)
+    let hatLink = hatList[randNum].base64
+    console.log(hatLink)
+
+    return Buffer.from(hatLink, "base64")
+}
+
+async function downloadBuffer(url) {
+    let resp = await fetch(url,{
+        method: 'GET',
+    })
+
+    // receive the response
+    let data = await resp.arrayBuffer()
+    return data
+}
+
+async function defaultBoss() {
+    //my fav boss ever --daniel
+    let johnKinmonth = await downloadBuffer("https://user-images.githubusercontent.com/69332964/128645143-86405a62-691b-4de9-8500-b9362675e1db.png");
+    johnKinmonth = Buffer.from(johnKinmonth)
+
+    return johnKinmonth
+}
--- grpc/manipulateService/manipulate_service.js
+++ grpc/manipulateService/manipulate_service.js
@@ -1,4 +1,4 @@
-var PROTO_PATH = __dirname + '/../proto/manipulate.proto';
+var PROTO_PATH = __dirname + '/manipulate.proto';
 
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
@@ -16,7 +16,18 @@ var manipulate_proto = grpc.loadPackageDefinition(packageDefinition).manipulate;
  * Implements the SayHello RPC method.
  */
 function manipulateService(call, callback) {
-    callback(null, {base64: 'Hello ' + call.request.username});
+    let face = call.request.face;
+    let hat = call.request.hat;
+
+    try {
+        result = await findFace(face)
+    } catch (e) {
+        res.send("Invalid image")
+        console.log(e)
+    }
+
+    let finalFace = await overlayHat(hat, result, face)
+    callback(null, {base64: finalFace});
 }
 
 /**
@@ -33,3 +44,44 @@ function main() {
 }
 
 main();
+
+const findFace = async (face) => {
+    const image = await canvas.loadImage(face)
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights')
+    // const Canvas = canvas.createCanvas(image.width, image.height)
+    // const ctx = Canvas.getContext('2d')
+    // ctx.drawImage(image, 0, 0, image.width, image.height)
+    // console.log(ctx)
+  
+    const fullFaceDescription = await faceapi.detectAllFaces(image)
+    // use await to retrieve face data
+  
+    let relData = fullFaceDescription[0]._box
+    console.log(`Detected faces: ${JSON.stringify(relData)}`)
+  
+    return relData;
+    // {"_x":225.59293228387833,"_y":122.78662695563085,"_width":183.89773482084274,"_height":181.8649869230835}
+  }
+  
+async function overlayHat(hat, result, face) {
+    let hatImg = await Jimp.read(hat);
+    const image = await Jimp.read(face);
+  
+    let width = result._width
+    let height = result._height
+    let left = result._x
+    let top = result._y
+    console.log(width, height, left, top)
+    //  BoundingBox.Width:      ${data.BoundingBox.Width}`)
+  
+    hatImg = await hatImg.resize(width, height)
+    hatImg = await hatImg.rotate(10)
+    
+    image.composite(hatImg, left - width*0.18, top - height*1.2, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacityDest: 1,
+      opacitySource: 0.9
+    })
+  
+    return await image.getBase64Async(Jimp.MIME_PNG)
+  }
